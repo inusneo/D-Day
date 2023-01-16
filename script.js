4ec38b9cb71e2b25626653634e83056c6cb76a21
Kim Yeongjin <inusneo@gmail.com>
2023-01-16 17:45:16 +0900

    [add] selector in script

---
+++ script.js
@@ -0,0 +1,5 @@
+const dateMaker = () => {
+  const inputYear = document.querySelector('#inputYear');
+  const inputMonth = document.querySelector('#inputMonth');
+  const inputDate = document.querySelector('#inputDate');
+}
