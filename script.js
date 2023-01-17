3ba1d8444372eefd37c6aeb9fd8845e7e24099eb
Kim Yeongjin <inusneo@gmail.com>
2023-01-17 17:03:38 +0900

    [edit] counterMaker

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
+const btnCount = document.querySelector('#btnCount');
+const btnReset = document.querySelector('#btnReset');
+
 const dateMaker = () => {
-  const inputYear = document.querySelector('#inputYear');
-  const inputMonth = document.querySelector('#inputMonth');
-  const inputDate = document.querySelector('#inputDate');
+  const inputYear = document.querySelector('#inputYear').value;
+  const inputMonth = document.querySelector('#inputMonth').value;
+  const inputDate = document.querySelector('#inputDate').value;
+
+  const dateForm = `${inputYear}-${inputMonth}-${inputDate}`;
+
+  return dateForm;
 }
+
+const counterMaker = () => {
+  const targetDateInput = dateMaker();
+  const nowDate = new Date();
+  const targetDate = new Date(targetDateInput).setHours(0, 0, 0, 0);
+  const remaining = (targetDate - nowDate) / 1000;
+
+  const remainingDate = Math.floor(remaining / 3600 / 24);
+  const remainingHours = Math.floor(remaining / 3600) % 24;
+  const remainingMin = Math.floor(remaining / 60) % 60;
+  const remainingSec = Math.floor(remaining) % 60;
+
+  console.log(remainingDate, remainingHours, remainingMin, remainingSec);
+}
+
+btnCount.addEventListener('click', () => {
+  counterMaker();
+});
